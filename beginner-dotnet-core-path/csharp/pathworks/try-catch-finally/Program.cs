fe73a163e0ef4cddef0f91d0d592f8a9359883b5
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-02 19:46:46 +0300

    try catch done

---
+++ beginner-dotnet-core-path/csharp/pathworks/try-catch-finally/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+try
+{
+    Console.WriteLine("Bir sayı giriniz:");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("girmiş olduğunuz sayı :" + sayi);
+}catch(Exception ex){
+    Console.WriteLine("Hata :" + ex.Message.ToString());
+}finally
+{
+    Console.WriteLine("işlem tamamlandı");
+}
+
+try{
+    int a = int.Parse(null);
+    int b = int.Parse("-200000000000000");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Boş değer girdiniz");
+    Console.WriteLine(ex);
+
+}
+catch (FormatException ex) {
+    Console.WriteLine("Format tipi uygun değil");
+    Console.WriteLine(ex);
+}
+catch (OverflowException ex){
+    Console.WriteLine("çok küçük veya çok büyük bir değer girdiniz");
+    Console.WriteLine(ex);
+}
