6577096bc5121c3a5e9931455d560ca5773a7c56
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-02 22:01:36 +0300

    switch case completed

--- beginner-dotnet-core-path/csharp/pathworks/if-else/Program.cs
+++ beginner-dotnet-core-path/csharp/pathworks/if-else/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int time = DateTime.Now.Hour;
+
+if(time >= 6 && time < 11){
+    Console.WriteLine("günaydın");
+}else if(time <= 18){
+    Console.WriteLine("iyi günler");
+}
+else{
+    Console.WriteLine("iyi geceler");
+}
