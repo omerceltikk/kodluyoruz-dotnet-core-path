ec76a75e3e4c209ce62814c4e2e26fcbc666434d
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-03 13:14:38 +0300

    while-foreach done

---
+++ beginner-dotnet-core-path/csharp/pathworks/while-foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+
+while(sayac <= sayi){
+
+    toplam += sayac;
+    sayac++;
+}
+Console.WriteLine(toplam/sayi);
+
+int character = 'a';
+while (character < 'z')
+{
+    Console.Write(character);
+    character ++;
+}
+string[] arabalar = {"Ford","Toyota","Nissan","BMW"};
+foreach (var item in arabalar)
+{
+    Console.WriteLine(item);
+}
