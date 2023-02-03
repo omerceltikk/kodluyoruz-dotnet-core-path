cc7462b6483b95aa104c371d7c866abc91c1b5eb
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-03 13:01:01 +0300

    forloop completed

---
+++ beginner-dotnet-core-path/csharp/pathworks/forloop-break-continue/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayac = int.Parse(Console.ReadLine());
+
+// for (int i = 0; i <= sayac; i++)
+// {
+//     if(i%2 == 1){
+//         Console.WriteLine(i);
+//     }
+// }
+// int tektoplam = 0;
+// int cifttoplam = 0;
+
+// for (int i = 0; i <= 1000; i++)
+// {
+//     if(i%2 ==1){
+//         tektoplam += i;
+//     }
+//     if(i%2 ==0){
+//         cifttoplam += i;
+//     }
+//     Console.WriteLine("tektoplam"+tektoplam);
+//     Console.WriteLine("cifttoplam"+cifttoplam);
+// }
+// for (int i = 1; i < 10; i++)
+// {
+//     if(i == 4)
+//     break;
+//     Console.WriteLine(i);
+// }
+int n = 6;
+ string toplam = "";
+
+for(int i = 1; i <=  n; i++){
+           var spaces = new string(Convert.ToChar(" "),n-i);
+           var symbol = new string(Convert.ToChar("#"),i);
+        }
--- beginner-dotnet-core-path/csharp/pathworks/switch-case/Program.cs
+++ beginner-dotnet-core-path/csharp/pathworks/switch-case/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int month = DateTime.Now.Month;
+
+//expression 
+switch (month)
+{
+    case 1:
+    Console.WriteLine("Ocak ayındasınız");
+    break;
+    case 2:
+    Console.WriteLine("Şubat ayındasınız.");
+    break;
+    case 4:
+    Console.WriteLine("nisan ayındasınız.");
+    break;
+    case 3:
+    Console.WriteLine("mart ayındasınız.");
+    break;
+
+    default:
+    break;
+}
+switch (month)
+{   case 12:
+    case 1:
+    case 2:
+     Console.WriteLine("Kış mevisimi");
+     break;
+     case 3:
+    case 4:
+    case 5:
+     Console.WriteLine("ilkbahar mevisimi");
+     break;
+
+    default:
+    break;
+}
