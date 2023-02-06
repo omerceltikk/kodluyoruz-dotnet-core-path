1c7f47c7e25954cf3416c777879b3ba64883829b
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-06 19:39:54 +0300

    methods 1 completed

--- beginner-dotnet-core-path/csharp/pathworks/array-methods/Program.cs
+++ beginner-dotnet-core-path/csharp/pathworks/array-methods/Program.cs
@@ -1,31 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
 
-string[] renkler = new string[5];
-string[] hayvanlar = {"kedi","köpek","kuş","maymun"};
-int[] dizi;
-dizi = new int[5];
+// string[] renkler = new string[5];
+// string[] hayvanlar = {"kedi","köpek","kuş","maymun"};
+// int[] dizi;
+// dizi = new int[5];
 
-renkler[0] = "mavi";
-Console.WriteLine(hayvanlar[0]);
-dizi[3] = 10;
+// renkler[0] = "mavi";
+// Console.WriteLine(hayvanlar[0]);
+// dizi[3] = 10;
 
-Console.WriteLine(dizi[3]);
-Console.WriteLine(hayvanlar[1]);
-Console.WriteLine(renkler[0]);
+// Console.WriteLine(dizi[3]);
+// Console.WriteLine(hayvanlar[1]);
+// Console.WriteLine(renkler[0]);
 
-int diziUzunlugu =int.Parse(Console.ReadLine());
-int[] sayilar = new int[diziUzunlugu]; 
+// int diziUzunlugu =int.Parse(Console.ReadLine());
+// int[] sayilar = new int[diziUzunlugu]; 
 
-for(int i=0; i< diziUzunlugu; i++)
-{
-    Console.Write("lütfen {0}. sayısı giriniz", i+1);
-    sayilar[i] = int.Parse(Console.ReadLine());
-}
-int toplam = 0;
-foreach (var item in sayilar)
-{
-    toplam += item;
+// for(int i=0; i< diziUzunlugu; i++)
+// {
+//     Console.Write("lütfen {0}. sayısı giriniz", i+1);
+//     sayilar[i] = int.Parse(Console.ReadLine());
+// }
+// int toplam = 0;
+// foreach (var item in sayilar)
+// {
+//     toplam += item;
 
-}
-Console.WriteLine("ortalama:" + toplam/diziUzunlugu);
+// }
+// Console.WriteLine("ortalama:" + toplam/diziUzunlugu);
+
+// foreach (var item in collection)
+// {
+    
+// }
+
+    
