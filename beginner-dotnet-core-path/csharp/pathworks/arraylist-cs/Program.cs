5b70830d5982d128f62f40ad3be7a9f342976e63
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-07 18:52:35 +0300

    arraylist done

--- beginner-dotnet-core-path/csharp/pathworks/arraylist-cs/Program.cs
+++ beginner-dotnet-core-path/csharp/pathworks/arraylist-cs/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;  
+using System.Collections;  
+using System.Linq;  
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ArrayList arlist = new ArrayList();
+        arlist.Add("ayşe");
+        arlist.Add(2);
+        arlist.Add(true);
+        arlist.Add('A');
+
+        Console.WriteLine(arlist[1]);
+
+        //AddRange - collection tipini ekleyebiliyoruz
+        string[] renkler = {"kırmızı","sarı","mavi"};
+        arlist.AddRange(renkler);
+        List<int> sayılar = new List<int>(){1,8,5,6,874,1,5,6,8,7,4};    
+        arlist.AddRange(sayılar);
+        
+        //Sort-arraydeki iki elemanı sortlayamıyor çünkü int32 bulmaya çalışıyor sadec sayılar olması mantıklı sortlamak için
+
+         //Binary Search -binary search için ilk olarak sortlamamız lazım  
+         arlist.BinarySearch(9);
+         //reverse
+         arlist.Reverse();
+         //Clear
+         arlist.Clear();
+}
+}
