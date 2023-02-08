de08b0880415850173b773cc2c4f5295ba847488
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-08 12:56:12 +0300

    hw1 completed

---
+++ beginner-dotnet-core-path/csharp/homeworks/hw1/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("TASK 1-lütfen n değişkenini giriniz.");
+uint n = uint.Parse(Console.ReadLine());
+uint[] dizi = new uint[n];
+
+for(int i = 0; i<n; i++){
+    Console.Write("lütfen {0}. sayyıyı giriniz",i+1);
+    dizi[i] = uint.Parse(Console.ReadLine());
+}
+for(int i = 0; i <n ;i++)
+  {
+    if(dizi[i] % 2 == 0){
+        Console.WriteLine(dizi[i]);
+    }
+  }
+Console.Write("TASK 1-Tamamlandı");
+
+Console.Write("TASK 2-Lütfen m değişkenini giriniz.");
+    uint m = uint.Parse(Console.ReadLine());
+   for (int i = 0; i < n; i++)
+   {
+    uint kalan = dizi[i] % m; 
+     if( dizi[i] == m){
+        Console.WriteLine(dizi[i]);
+    }else if(kalan == 0){
+             Console.WriteLine(dizi[i]);
+    }
+   }
+Console.Write("TASK 2-Tamamlandı");
+
+Console.Write("TASK 3-Lütfen n sayısı kadar kelime giriniz");
+string[] kelimeDizi = new string[n];
+for(uint i = 0; i < n; i++){
+    Console.Write("lütfen {0}. kelimeyi giriniz",i+1);
+    kelimeDizi[i] = Convert.ToString(Console.ReadLine());
+}
+// kelimeDizi.Reverse();
+for (int i = kelimeDizi.Length; i >= 0; i--)
+{
+    Console.WriteLine(kelimeDizi[i]);
+}
+    Console.Write("TASK 3-Tamamlandı");
+
+    Console.Write("TASK 4-Lütfen bir cümle giriniz");
+    string cumle = Convert.ToString(Console.ReadLine());
+    string[] words = cumle.Split(' ');
+    Console.WriteLine("Words length:" + words.Length);
+    string wordsJoined = string.Join("",words);
+    char[] charac = wordsJoined.ToCharArray();
+    Console.WriteLine("Character length:" + charac.Length);
+   
---
+++ beginner-dotnet-core-path/csharp/pathworks/dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Dictionary<Key_Veri_Tipi, Value_Veri_Tipi> dictionary_adi = new Dictionary<Key_Veri_Tipi, Value_Veri_Tipi>();
+
+Dictionary<int,string> kullanicilar = new Dictionary<int, string>();
+kullanicilar.Add(10,"ayşe");
+kullanicilar.Add(12,"ahmet");
+kullanicilar.Add(18,"deniz");
+kullanicilar.Add(20,"özcan");
+Console.WriteLine(kullanicilar[12]);
+foreach( var item in kullanicilar)
+{
+    Console.WriteLine(item);
+}
+
+//Count
+Console.WriteLine(kullanicilar.Count);
+//Contains
+Console.WriteLine(kullanicilar.ContainsKey(12));
+Console.WriteLine(kullanicilar.ContainsValue("ahmet"));
+
+//remove
+kullanicilar.Remove(12);
