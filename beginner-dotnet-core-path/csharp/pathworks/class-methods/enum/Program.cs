d8763602be3dd58d613a3857670375d42eb3804a
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-08 19:15:05 +0300

    enum done

--- beginner-dotnet-core-path/csharp/pathworks/class-methods/enum/Program.cs
+++ beginner-dotnet-core-path/csharp/pathworks/class-methods/enum/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(Gunler.Pazar);
+Console.WriteLine((int)Gunler.Cumartesi);
+int sicaklik = 32;
+if(sicaklik <= (int)HavaSicakligi.normal){
+    Console.WriteLine("Dışarıya çıkmak için havanın biraz daha sınımasını bekleyin");
+}else if( sicaklik >= (int)HavaSicakligi.sıcak){
+    Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+}else if(sicaklik >= (int)HavaSicakligi.normal && sicaklik<(int)HavaSicakligi.coksıcak){
+    Console.WriteLine("haydi dışarı çıkalım");
+
+}
+enum Gunler{
+    Pazartesi=1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaSicakligi{
+    soguk = 5,
+    normal = 20,
+    sıcak = 25,
+    
+    coksıcak=30
+}
