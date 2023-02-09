a002ffe0da46a00316483d0b8fe176daf1b8ae29
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-09 12:49:29 +0300

    triangle completed

---
+++ beginner-dotnet-core-path/csharp/csharp-projects/drawtriangle-project2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i<n ; i++){
+   for(int k = i; k<n ; k++){
+    Console.Write(" ");
+   }
+   for(int x=0; x <= i; x++){
+    Console.Write("*");
+    Console.Write(" ");
+   }
+   Console.WriteLine();
+}
+Console.Read();
