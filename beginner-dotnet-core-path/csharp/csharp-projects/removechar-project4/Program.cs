8be8d72b0919eee98b1588029f60953b690dee0b
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-09 13:14:56 +0300

    project4 completed

---
+++ beginner-dotnet-core-path/csharp/csharp-projects/removechar-project4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+ Console.WriteLine("Lütfen bir kelime giriniz ve ardından çıkarmak istediğiniz karakterin sayısını giriniz(Aralarında virgül kullanınız.):");
+string word = Console.ReadLine();
+string[] arr = word.Split(",");
+string word2 = Convert.ToString(arr[0]);
+int numb = Convert.ToInt32(arr[1]);
+string lastOut = word2.Remove(numb-1,1);
+
+Console.WriteLine(lastOut);
+
+
