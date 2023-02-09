d76cac6807a673156dc6a7b7a80d652ae199465a
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-09 18:52:22 +0300

    absolute squaring done

---
+++ beginner-dotnet-core-path/csharp/csharp-projects/absolutesquaring-projecr8/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bir sayı giriniz");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+int kucukToplam = 0;
+int buyukToplam = 0;
+Console.WriteLine("Lütfen sayılarınızı giriniz");
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("{0}. sayınız", i + 1);
+    arr[i] = int.Parse(Console.ReadLine());
+}
+for(int i = 0; i <arr.Length; i++){
+    if(arr[i] > 67){
+        int fark = arr[i]-67;
+        buyukToplam += fark*fark;
+    }
+    else if(arr[i] < 67){
+        kucukToplam += 67-arr[i];
+    }
+}
+Console.WriteLine("67den büyük sayıların mutlak kareleri toplamı:"+buyukToplam);
+Console.WriteLine("67den küçük sayıların 67 ile farkları toplamı:"+kucukToplam);
+
