d03e07adad97c77beb3a61f9d80138bcbb65292b
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-09 13:22:05 +0300

    reversechar completed

---
+++ beginner-dotnet-core-path/csharp/csharp-projects/reversechar-project5/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Lütfen bir cümle giriniz.");
+string sentence = Console.ReadLine();
+
+char[] arr = sentence.ToCharArray();
+Array.Reverse(arr);
+string result = string.Join("",arr);
+Console.WriteLine(arr);
