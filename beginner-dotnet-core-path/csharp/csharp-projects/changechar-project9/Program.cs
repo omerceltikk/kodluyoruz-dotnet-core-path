6f49915d875d013ad16d5f2b70a09619755d6792
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-10 11:42:59 +0300

    change char completed

---
+++ beginner-dotnet-core-path/csharp/csharp-projects/changechar-project9/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lütfen bir cümle giriniz");
+string[] arr = Console.ReadLine().Split(" ");
+foreach (var item in arr)
+{
+    char first = item[0];
+    char last = item[item.Length-1];
+    string sentence = last + item.Substring(1, item.Length-2) + first;
+    Console.Write(sentence+ " ");
+}
