fb8040ebb0ae1326bb13e5f69115444e4e7e0e1d
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2023-02-10 11:43:08 +0300

    change char completed

--- beginner-dotnet-core-path/csharp/csharp-projects/changechar-project9/Program.cs
+++ beginner-dotnet-core-path/csharp/csharp-projects/changechar-project9/Program.cs
@@ -6,5 +6,5 @@ foreach (var item in arr)
     char first = item[0];
     char last = item[item.Length-1];
     string sentence = last + item.Substring(1, item.Length-2) + first;
-    Console.Write(sentence+ " ");
+    Console.Write(sentence + " ");
 }
